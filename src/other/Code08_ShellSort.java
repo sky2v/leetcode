4ff3d8791a7fd94127e8b055de1672c64a632f00
liujian <liujian@kilox.cn>
2024-04-03 21:22:14 +0800

    add：希尔排序 && LURcache

--- src/other/Code08_ShellSort.java
+++ src/other/Code08_ShellSort.java
@@ -1,4 +1,39 @@
 package other;
 
+/**
+ * 希尔排序的基本思想是：先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录 “基本有序” 时，再对全体记录进行依次直接插入排序
+ * 具体算法描述：
+ *      选择一个增量序列 {t1,t2,…,tk}，其中 ti>tj,i<j,tk=1；
+ *      按增量序列个数 k，对序列进行 k 趟排序；
+ *      每趟排序，根据对应的增量 t，将待排序列分割成若干长度为 m 的子序列，分别对各子表进行直接插入排序。仅增量因子为 1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ *
+ */
 public class Code08_ShellSort {
+
+    /**
+     * 希尔排序
+     *
+     * @param arr
+     * @return arr
+     */
+    public static int[] shellSort(int[] arr) {
+        int n = arr.length;
+        int gap = n / 2;
+        while (gap > 0) {
+            for (int i = gap; i < n; i++) {
+                int current = arr[i];
+                int preIndex = i - gap;
+                // Insertion sort
+                while (preIndex >= 0 && arr[preIndex] > current) {
+                    arr[preIndex + gap] = arr[preIndex];
+                    preIndex -= gap;
+                }
+                arr[preIndex + gap] = current;
+
+            }
+            gap /= 2;
+        }
+        return arr;
+    }
+
 }
--- src/other/Code12_LRU.java
+++ src/other/Code12_LRU.java
@@ -1,4 +1,73 @@
 package other;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Code12_LRU {
+
+    /**
+     * 继承 LinkedHashMap
+     */
+    static class LRUCache extends LinkedHashMap {
+
+        /**
+         * 容量
+         */
+        private int capacity;
+
+        /**
+         * 调用父类的构造方法
+         * capacity：容量
+         * 0.75F：负载因子
+         * true：开启 LRU 缓存算法
+         */
+        public LRUCache(int capacity) {
+            super(capacity, 0.75F, true);
+            this.capacity = capacity;
+        }
+
+        /**
+         * 获取元素，调用父类的构造方法
+         * 原理：
+         * 	（1）首先将这个元素从队列中移除
+         *	（2）然后再将这个元素添加到队列的头部
+         */
+        public int get(int key) {
+            return (int) super.getOrDefault(key, -1);
+        }
+
+        /**
+         * 得到元素，调用父类的构造方法
+         * 原理：
+         * 	（1）首先判断 key 所代表的元素是否已经在队列中
+         * 	（2）如果在队列中，则直接覆盖元素的值即可
+         *	（3）覆盖之后则从队列中移除，然后再添加到队列头部
+         * 	（4）此时操作完成
+         * 	（5）如果不在队列中，则判断容量是否足够
+         * 	（6）如果容量足够，则直接添加到队列头部，此时操作完成
+         *	（7）如果容量不足够，则移除队列最后一个元素
+         * 	（8）并且将要添加的元素添加到队列的头部，此时操作完成
+         */
+        public void put(int key, int value) {
+            super.put(key, value);
+        }
+
+        /**
+         * 移除元素条件方法，因为在 LinkedHashMap 中是空方法
+         * 所以我们需要重写方法，定义自己的逻辑
+         * 这里就是当容量不够时进行移除元素
+         */
+        @Override
+        public boolean removeEldestEntry(Map.Entry eldest) {
+            return super.size() > capacity;
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(10);
+    }
+
+
+
 }
