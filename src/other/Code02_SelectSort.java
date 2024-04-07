66b663be12910b272ff982ffc72709a238a3d804
sky <1061399675@qq.com>
2024-04-07 23:20:17 +0800

    fix: 修改选择排序为正确的排序

--- src/other/Code02_SelectSort.java
+++ src/other/Code02_SelectSort.java
@@ -14,12 +14,14 @@ public class Code02_SelectSort {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    swap(arr, i, j);
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+            swap(arr, i, minIndex);
         }
     }
 
